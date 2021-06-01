61cb4781214c7ed7fa1335ee563f035e93b1c59b
Abdy Franco <yosoy@abdyfran.co>
2021-06-01 16:37:36 -0600

    Initial release

---
+++ config/generic_domain.php
@@ -0,0 +1,15 @@
+<?php
+// Email templates
+Configure::set('GenericDomain.email_templates', [
+    'en_us' => [
+        'lang' => 'en_us',
+        'text' => 'Your new domain is being processed and will be registered soon!
+
+Domain: {service.domain}
+
+Thank you for your business!',
+        'html' => '<p>Your new domain is being processed and will be registered soon!</p>
+<p>Domain: {service.domain}</p>
+<p>Thank you for your business!</p>'
+    ]
+]);
---
+++ generic_domain.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Generic Domain Registrar Module
+ *
+ * @package blesta
+ * @subpackage blesta.components.modules.generic_domain
+ * @copyright Copyright (c) 2021, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+class GenericDomain extends RegistrarModule
+{
+    /**
+     * Initializes the module
+     */
+    public function __construct()
+    {
+        // Load the language required by this module
+        Language::loadLang('generic_domain', null, dirname(__FILE__) . DS . 'language' . DS);
+
+        // Load module config
+        $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
+    }
+
+    /**
+     * Returns the rendered view of the manage module page
+     *
+     * @param mixed $module A stdClass object representing the module and its rows
+     * @param array $vars An array of post data submitted to or on the manager module
+     *  page (used to repopulate fields after an error)
+     * @return string HTML content containing information to display when viewing the manager module page
+     */
+    public function manageModule($module, array &$vars)
+    {
+        // Load the view into this object, so helpers can be automatically added to the view
+        $this->view = new View('manage', 'default');
+        $this->view->base_uri = $this->base_uri;
+        $this->view->setDefaultView('components' . DS . 'modules' . DS . 'generic_domain' . DS);
+
+        // Load the helpers required for this view
+        Loader::loadHelpers($this, ['Html', 'Widget']);
+
+        $this->view->set('module', $module);
+
+        return $this->view->fetch();
+    }
+
+    /**
+     * Adds the service to the remote server. Sets Input errors on failure,
+     * preventing the service from being added.
+     *
+     * @param stdClass $package A stdClass object representing the selected package
+     * @param array $vars An array of user supplied info to satisfy the request
+     * @param stdClass $parent_package A stdClass object representing the parent
+     *  service's selected package (if the current service is an addon service)
+     * @param stdClass $parent_service A stdClass object representing the parent
+     *  service of the service being added (if the current service is an addon
+     *  service and parent service has already been provisioned)
+     * @param string $status The status of the service being added. These include:
+     *  - active
+     *  - canceled
+     *  - pending
+     *  - suspended
+     * @return array A numerically indexed array of meta fields to be stored for this service containing:
+     *  - key The key for this meta field
+     *  - value The value for this key
+     *  - encrypted Whether or not this field should be encrypted (default 0, not encrypted)
+     * @see Module::getModule()
+     * @see Module::getModuleRow()
+     */
+    public function addService(
+        $package,
+        array $vars = null,
+        $parent_package = null,
+        $parent_service = null,
+        $status = 'pending'
+    )
+    {
+        $meta = [];
+        foreach ($vars as $key => $value) {
+            $meta[] = [
+                'key' => $key,
+                'value' => $value,
+                'encrypted' => 0
+            ];
+        }
+
+        return $meta;
+    }
+
+    /**
+     * Edits the service on the remote server. Sets Input errors on failure,
+     * preventing the service from being edited.
+     *
+     * @param stdClass $package A stdClass object representing the current package
+     * @param stdClass $service A stdClass object representing the current service
+     * @param array $vars An array of user supplied info to satisfy the request
+     * @param stdClass $parent_package A stdClass object representing the parent
+     *  service's selected package (if the current service is an addon service)
+     * @param stdClass $parent_service A stdClass object representing the parent
+     *  service of the service being edited (if the current service is an addon service)
+     * @return array A numerically indexed array of meta fields to be stored for this service containing:
+     *  - key The key for this meta field
+     *  - value The value for this key
+     *  - encrypted Whether or not this field should be encrypted (default 0, not encrypted)
+     * @see Module::getModule()
+     * @see Module::getModuleRow()
+     */
+    public function editService($package, $service, array $vars = [], $parent_package = null, $parent_service = null)
+    {
+        return $this->addService($package, $vars, $parent_package, $parent_service);
+    }
+
+    /**
+     * Verifies that the provided domain name is available
+     *
+     * @param string $domain The domain to lookup
+     * @param int $module_row_id The ID of the module row to fetch for the current module
+     * @return bool True if the domain is available, false otherwise
+     */
+    public function checkAvailability($domain, $module_row_id = null)
+    {
+        if (class_exists('\Iodev\Whois\Factory')) {
+            $whois = \Iodev\Whois\Factory::get()->createWhois();
+
+            try {
+                return $whois->isDomainAvailable($domain);
+            } catch (Exception $e) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
